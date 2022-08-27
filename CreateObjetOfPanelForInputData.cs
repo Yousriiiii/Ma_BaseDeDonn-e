58065248062430c6a3314bf6a5653d39ae44be80
Yousri <54667@etu.he2b.be>
2022-08-27 02:03:45 +0200

    Mise des logos pour modification de donnée

--- CreateObjetOfPanelForInputData.cs
+++ CreateObjetOfPanelForInputData.cs
@@ -20,10 +20,12 @@ namespace Ma_BaseDeDonnée
         {
             // panelOfData reprèsente le panel qui englobe le label et le textbox
             // line est le titre de la donné (première ligne)
+
+            // Màj - ajouter une image qui permettra d'ouvrir un menu contextuel (dans une fenêtre d'outils)
             #region Label Part
             Label ColomnName = new Label();
             ColomnName.AutoSize = true;
-            ColomnName.Location = new Point(100, 25);
+            ColomnName.Location = new Point(100, 50);
             ColomnName.Name = "NameOfColumnLabel";
             ColomnName.Size = new Size(200, 10);
             ColomnName.Text = line + " :";
@@ -33,12 +35,25 @@ namespace Ma_BaseDeDonnée
             #region TextBox Part
 
             TextBox TextBoxInputData = new TextBox();// Ce sera la textbox où il y aura la data à ajouter
-            TextBoxInputData.Location = new Point(200, 25);
+            TextBoxInputData.Location = new Point(200, 50);
             TextBoxInputData.Name = "NameOfTextBox";
             TextBoxInputData.Size = new Size(100, 100);
             #endregion
 
+            #region Logo
 
+            PictureBox pictureBox = new PictureBox();
+            pictureBox.Image = global::Ma_BaseDeDonnée.Properties.Resources.Logo_Modificateur;
+            pictureBox.InitialImage = global::Ma_BaseDeDonnée.Properties.Resources.Logo_Modificateur;
+            pictureBox.Location = new Point(1200 , 25);
+            pictureBox.Size = new Size(50,50);
+            pictureBox.SizeMode = PictureBoxSizeMode.StretchImage; // Je spécifie le mode de réglage de dimension
+                                                                   // le mode utilise (stretchimage) est un mode qui va permettre
+                                                                   // d'élargir l'image selon la taille de picturebox
+            pictureBox.BorderStyle = BorderStyle.Fixed3D;
+            #endregion
+
+            panelOfData.Controls.Add(pictureBox);
             panelOfData.Controls.Add(ColomnName); // C'est pour le label de chaque panel
             panelOfData.Controls.Add(TextBoxInputData);
 
