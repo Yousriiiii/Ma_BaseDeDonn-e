3f6215ecbede026b513e983d93a01694e3dfecad
Yousri <54667@etu.he2b.be>
2022-08-31 14:11:18 +0200

    Début d'une nouvelle Màj -> Possibilité de faire des recherches dans la base de donnée

---
+++ ResearchData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ma_BaseDeDonnée
+{
+    public class ResearchData
+    {
+        public string path; // C'est le chemin d'accés
+        public string Header; // Le type de donnée
+        public ResearchData(string path,string header) { this.path = path; this.Header = header; }
+
+        // BUT : Pouvoir réaliser des recherches d'information selon le type de donné (exemple : nom, prénom,...) 
+        // Cette classe doit pouvoir avoir comme entré, ce qu'il y a dans la textbox, le type de donnée et le chemin d'accés
+        // , et en sortie, le résultat des recherches
+
+        public List<string> ResearchList { get; set; } // C'est la liste qui correspond au résultat des recherche
+
+        public void Research(string item) // Item représente ce qu'il y a dans la textbox
+        {
+
+        }
+    }
+}
